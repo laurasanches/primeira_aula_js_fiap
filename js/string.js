ee4c4517e1847212cf8dfdd413fb9333d560ec24
Laura Sanches <lalinhabom@gmail.com>
2022-03-08 17:12:57 -0300

    Aprendendo conceitos básicos de JavaScript

---
+++ js/string.js
@@ -0,0 +1,28 @@
+let firstNumber = 10;
+let secondNumber = '10';
+
+console.log(`${firstNumber} == ${secondNumber} = ${firstNumber == secondNumber}`)
+console.log(`${firstNumber} == ${secondNumber} = ${firstNumber === secondNumber}`)
+
+firstNumber = 10;
+secondNumber = 3;
+console.log(`${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
+console.log(`${firstNumber} % ${secondNumber} = ${firstNumber % secondNumber}`);
+
+// objeto string
+const text = 'JS é uma linguagem de programação fantástica'
+console.log(text);
+
+// tamanho do texto
+console.log(text.length);
+console.log(text[5]);
+
+// maiúsculas e minúsculas
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+
+// pegar pedaço da string slice (onde começa e onde termina)
+let pedacoString = text.slice(3,8)
+console.log(pedacoString);
+
+// parei no minuto 1:33:26 da aula
